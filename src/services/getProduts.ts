bc398a5194e149e97b0333045b05eb934d6ab876
Azuki19 <analucia27259@gmail.com>
2024-04-28 22:14:11 -0500

    feat

--- src/services/getProduts.ts
+++ src/services/getProduts.ts
@@ -1,6 +1,7 @@
 export const productData = async () => {
 	try {
 		const data = await fetch('https://fakestoreapi.com/products').then((res) => res.json());
+		console.log(data);
 		return data;
 	} catch (error) {
 		console.error('Error al obtener los productos', error);
